87bc575de532d20c5c4a8eefc9f499731ebc4317
Sharuk19 <sharuk2003@gmail.com>
2025-06-15 13:58:46 +0530

    updated set method for accnum

--- BankingSystem.java
+++ BankingSystem.java
@@ -2,4 +2,7 @@ public class BankingSystem {
   private double accountnumber;
   private String holdername;
   private double balance;
+  public void setAccountnumber(double accountnumber){
+      this.accountnumber=accountnumber;
+  }
 }
